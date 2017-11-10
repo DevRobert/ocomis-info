b21bf285407f93288ad96af5540eb451f814c491
Robert Blutner <robert@blutner.de>
2017-11-10 18:55:48 +0100

    Docker Setup

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const Hapi = require("hapi")
 const server = new Hapi.Server()
 
 server.connection({
-    host: "localhost",
     port: 3000
 })
 
@@ -29,5 +28,6 @@ server.start((error) => {
         throw error
     }
 
+    console.log("Ocomis info service started.")
     console.log("Server running at: " + server.info.uri)
 })
