9b5b750e33ebde573a4d7874d76e2780980b3f3d
Robert Blutner <robert@blutner.de>
2017-12-17 17:01:17 +0100

    Logging

---
+++ index.js
@@ -0,0 +1,23 @@
+const Hapi = require('hapi')
+const HapiPino = require('hapi-pino')
+const routes = require('./lib/routes/')
+
+const server = new Hapi.Server({
+    port: 3000
+})
+
+server.route(routes)
+
+async function provision() {
+    await server.register(HapiPino)
+
+    await server.start()
+
+    server.logger().info('Ocomis info service started.')
+    server.logger().info(`Server running at: ${server.info.uri}`)
+}
+
+provision().catch((error) => {
+    console.error(error)
+    process.exit(1)
+})
